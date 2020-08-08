8fbd3334585db37b671b29d6afaac2865867f10c
Raymoun17 <raymoun17@gmail.com>
2020-08-08 07:08:07 -0400

    fixed small bug where page would redirect if any key was pressed

--- js/hotkeys.js
+++ js/hotkeys.js
@@ -1,5 +1,5 @@
 //A dictionary containing the sites with the corresponding hotkeys
-const sites = {"R":"https://www.reddit.com", "T":"http://www.4channel.org/g/", "D":"http://www.discord.com/login"}
+const sites = { "R": "https://www.reddit.com", "T": "http://www.4channel.org/g/", "D": "http://www.discord.com/login" }
 
 
 // social stuff
@@ -7,8 +7,11 @@ window.addEventListener('keydown', (event) => {
     //  Get the ASCII key code (int) of the key pressed then convert it to (string) the corresponding letter name
     let pressed_key = String.fromCharCode(event.keyCode);
     event.preventDefault();
-    //  Lookup the corresponding key value in the dictionary
-    let target_site = sites[pressed_key];
-    //  Change windows location to the corresponding site
-    window.location.href = target_site;
+    let keys = Object.keys(sites);
+    if (keys.includes(pressed_key)) {
+        //  Lookup the corresponding key value in the dictionary
+        let target_site = sites[pressed_key];
+        //  Change windows location to the corresponding site
+        window.location.href = target_site;
+    }
 })
